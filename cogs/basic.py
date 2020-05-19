99a3c754f5fd9dd0a5a5e2ccb6a9394d2b61e6c8
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2020-05-19 20:43:08 +0530

    updated basic.py and deleted utility.py
    1. basic.py
      a. deleted the info command, served no purose.
      b. updated the server and the user command.
    
    2. deleted the utility.py, urban command didnt work.

--- cogs/basic.py
+++ cogs/basic.py
@@ -22,36 +22,17 @@ class Basic(commands.Cog):
         return
 
     @commands.command(
-        name='info',
-        description='Info about the bot',
-        aliases=['bot', 'about']
-    )
-    async def info_command(self, ctx):
-        embed = discord.Embed(title="TestBotPy", description="Test bot with python" ,color = 0xf5f5dc)
-        embed.add_field(name= "Name " , value="TestBotPy" , inline=True)
-        embed.add_field(name= "Prefix for commands" , value = "!, t!, ==" , inline =True)
-        embed.set_footer(text = "Feel free to provide suggestions to my owner!")
-        await ctx.send(embed=embed)
-
-        return
-
-    @commands.command(
         name='server',
         description='Get the guild/server info.',
-        aliases=['guild', 'serverinfo']
+        aliases=['guild', 'guildinfo', 'serverinfo']
     )
     async def server_command(self, ctx):
-        embed = discord.Embed(title="{}'s info".format(ctx.message.guild.name), description="Here's what I could find in my bag...", color=0xf5f5dc)
-        embed.add_field(name="Servername", value=ctx.message.guild.name, inline=True)
-        embed.add_field(name="ID", value=ctx.message.guild.id, inline=True)
-        embed.add_field(name="Verification Level", value=ctx.message.guild.verification_level, inline=True)
-        embed.add_field(name="Server Region", value=ctx.message.guild.region, inline=True)
-        embed.add_field(name="Owner" , value=ctx.message.guild.owner, inline=True)
-        embed.add_field(name="Channels",value=len(ctx.message.guild.channels))
-        embed.add_field(name="Roles", value=len(ctx.message.guild.roles))
-        embed.add_field(name="Members", value=len(ctx.message.guild.members))
-        embed.add_field(name="Emojis", value=len(ctx.message.guild.emojis))
+        servercreate = ctx.message.guild.created_at
+        embed = discord.Embed(title=f"**{ctx.message.guild.name} [{ctx.message.guild.id}]**", color=0xf5f5dc)
         embed.set_thumbnail(url=ctx.message.guild.icon_url)
+        embed.add_field(name="Members", value=f"> **{len(ctx.message.guild.members)}** members\n > Owner: **{ctx.message.guild.owner}** (ID: **{ctx.message.guild.owner.id})**", inline=False)
+        embed.add_field(name="Other", value=f'> Roles: **{len(ctx.message.guild.roles)}**\n > Region: **{ctx.message.guild.region}**\n > Created At: **{servercreate.strftime("%d/%m/%Y, %H:%M:%S")}**\n > Verification Level: **{ctx.message.guild.verification_level}**')
+        embed.add_field(name='Misc', value=f"> Channels: **{len(ctx.message.guild.channels)}**\n > Emojis: **{len(ctx.message.guild.emojis)}**", inline=False)
         await ctx.send(embed=embed)
 
         return
@@ -64,22 +45,23 @@ class Basic(commands.Cog):
     )
     async def user(self, ctx, user: discord.Member = None):
         user = user or ctx.message.author
+        if user.bot == True:
+            ubot = "Yes"
+        else:
+            ubot = "No"
         try:
             join_date = user.joined_at
             create_date = user.created_at
-            embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find in my bag...", color=0xf5f5dc)
-            embed.add_field(name="Username", value=user.name, inline=True)
-            embed.add_field(name="Nickname" , value = user.nick , inline = True)
-            embed.add_field(name="ID", value=user.id, inline=True)
-            embed.add_field(name="Bot", value=user.bot ,inline=True)
-            embed.add_field(name="Status", value=user.status,inline=True)
-            embed.add_field(name="Highest role", value=user.top_role)
-            embed.add_field(name="Ceated At", value=join_date.strftime("%d/%m/%Y, %H:%M:%S"))
-            embed.add_field(name="Joined At" , value=create_date.strftime("%d/%m/%Y, %H:%M:%S"))
+            embed = discord.Embed(title="Here is {}'s info".format(user.name), color=0xf5f5dc)
+            embed.add_field(name='Here are the details', value=f'> ID: **{user.id}**\n > Tag: **{user.name}#{user.discriminator}**\n > Nickname: **{user.nick}**\n > Created At: **{create_date.strftime("%d/%m/%Y, %H:%M:%S")}**\n > Joined At: **{join_date.strftime("%d/%m/%Y, %H:%M:%S")}**\n > Bot: **{ubot}**\n > Status: **{user.status}**')
             embed.set_thumbnail(url=user.avatar_url)
+            embed.set_footer(
+                text=f'{ctx.message.guild.name}',
+                icon_url=ctx.message.guild.icon_url
+            )
             await ctx.send(embed=embed)
-        except:
-            await ctx.send("Error")
+        except Exception as e:
+            await ctx.send(f'There was an error: {e}')
 
         return
 
